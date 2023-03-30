92065aac33003e386d8ed1fd4cf3f0c34442832a
admi_panle_hope <githubaddmin@gmail.com>
2023-03-30 16:42:49 +0530

    data

--- src/pages/Event/EventPage.js
+++ src/pages/Event/EventPage.js
@@ -14,7 +14,17 @@ export default function EventPage() {
   const [holidayEvent,setHolidayEvent]=useState()
 
 const getFunction=()=>{
-
+  const colorData = [
+    '#2196f3',
+    '#31dc7f',
+    '#2cc0c1', 
+    '#dc3131', 
+    '#d25ee6', 
+    '#dc9931', 
+    '#ffeb3b', 
+    '#307cbf'
+]
+  
   const BASE_CALENDAR_URL = "https://www.googleapis.com/calendar/v3/calendars";
   const BASE_CALENDAR_ID_FOR_PUBLIC_HOLIDAY =
     "holiday@group.v.calendar.google.com"; // Calendar Id. This is public but apparently not documented anywhere officialy.
@@ -22,14 +32,20 @@ const getFunction=()=>{
   const CALENDAR_REGION = "en.indian";
   const url = `${BASE_CALENDAR_URL}/${CALENDAR_REGION}%23${BASE_CALENDAR_ID_FOR_PUBLIC_HOLIDAY}/events?key=${API_KEY}`
   
+  
   fetch(url).then(response => response.json()).then(data => {
   const holidays = data.items;
   const holidayData=holidays?.map((item)=>{
  item?.end?.date
-const getDateData={"end":item?.end?.date}
+ const random = Math.ceil(Math.random() * 7);
+  const dataColor= colorData[random];
+  console.log("dataColor",dataColor)
+
+ const eventColor={"color":dataColor}
+ const getDateData={"end":item?.end?.date}
 const getDateDAtaDAta={"start":item?.start?.date}
 const getHolidayName={"title":item?.summary}
- const mergeObject={...getDateData,...getDateDAtaDAta,...getHolidayName}
+ const mergeObject={...getDateData,...getDateDAtaDAta,...getHolidayName,...eventColor}
  return mergeObject
 })
 if(holidayData){
@@ -74,6 +90,13 @@ const apiPostFunction=()=>{
     }
   }
   function renderEventContent(eventInfo) {
+    const liveDate=new Date()
+    const eventData=eventInfo?.event?._instance?.range?.end
+    const dataAdd=[]
+    const dataMerge={"timeText":eventInfo?.timeText}
+      // eventInfo?.push()
+      // const filterData=dataAdd?.map((item)=>{item})
+      console.log("eve",eventInfo)
     return (
       <>
         <b>{eventInfo.timeText}</b>
@@ -119,37 +142,37 @@ const handleWeekendsToggle = () => {
   
   return (
     <div className='event-page'>
-        <Container>
           <button onClick={getFunction}>click</button>
       {renderSidebar()}
     
-  
-  <FullCalendar
-  plugins={[dayGridPlugin,timeGridPlugin, interactionPlugin]}
-  headerToolbar={{
-    left: 'prev,next today',
-    center: 'title',
-    right: 'dayGridMonth,timeGridWeek,timeGridDay'
-  }}
-  initialView='dayGridMonth'
-  editable={true}
-  selectable={true}
-  selectMirror={true}
-  initialEvents={holidayEvent}
-  dayMaxEvents={true}
-  weekends={weekendsVisible}
-  // initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
-  select={handleDateSelect}
-  eventContent={renderEventContent} // custom render function
-  eventClick={handleEventClick}
-  eventsSet={handleEvents} // called after events are initialized/added/changed/removed
-  /* you can update a remote database when these fire:
-  eventAdd={function(){}}
-  eventChange={function(){}}
-  eventRemove={function(){}}
-  */
-/>
-        </Container>
+  {
+    holidayEvent? <FullCalendar
+    plugins={[dayGridPlugin,timeGridPlugin, interactionPlugin]}
+    headerToolbar={{
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    }}
+    initialView='dayGridMonth'
+    editable={true}
+    selectable={true}
+    selectMirror={true}
+    initialEvents={holidayEvent}
+    dayMaxEvents={true}
+    weekends={weekendsVisible}
+    // initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+    select={handleDateSelect}
+    eventContent={renderEventContent} // custom render function
+    eventClick={handleEventClick}
+    eventsSet={handleEvents} // called after events are initialized/added/changed/removed
+    /* you can update a remote database when these fire:
+    eventAdd={function(){}}
+    eventChange={function(){}}
+    eventRemove={function(){}}
+    */
+  />:""
+  }
+ 
     </div>
   )
 }
